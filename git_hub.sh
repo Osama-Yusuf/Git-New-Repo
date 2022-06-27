5630bc996d8a082e0f225919d28f4ad744c31452
osama-yusuf <osama9mohamed5@gmail.com>
2022-06-28 01:27:11 +0200

    updated git_hub.sh

--- git_hub.sh
+++ git_hub.sh
@@ -123,6 +123,67 @@ modify_branch() {
 }
 modify_branch
 
+modify_remote() {
+    echo
+    git remote
+    git remote -v
+    echo
+
+    another_remote_fun() {
+        read -p "DO you want to perform another remote repository modification? (y/n) " another_remote
+        if [ "$another_remote" = "y" ]; then
+            modify_remote
+        elif [ "$another_remote" = "n" ]; then
+            clear
+        else
+            clear
+            echo "Invalid input, Please enter y or n"
+            echo
+            another_remote_fun
+        fi
+    }
+
+    read -p "Do you want to add or remove a remote repository? (y/n) " answer_remote
+    if [ "$answer_remote" = "y" ]; then
+        read -p "what do you want to do (a)dd or (r)emove a remote repository? (a/r) " answer_add_remove
+        if [ "$answer_add_remove" = "a" ]; then
+            echo
+            read -p "Enter remote repository name you want to add: " remote_repository_name
+            read -p "Enter remote repository URL you want to add: " remote_repository_URL
+            echo
+            git remote add $remote_repository_name $remote_repository_URL
+            clear
+            echo "$remote_repository_name added"
+            another_remote_fun
+        elif [ "$answer_add_remove" = "r" ]; then
+            echo
+            read -p "Enter remote repository name you want to remove: " remote_repository_name
+            echo
+            git remote rm $remote_repository_name
+            clear
+            echo "$remote_repository_name removed"
+            another_remote_fun
+        else
+            clear
+            echo "Invalid input, Please enter a or r"
+            modify_remote
+        fi
+
+        echo
+        git remote
+        git remote -v
+        echo
+
+    elif [ "$answer_remote" = "n" ]; then
+        clear
+        echo "No remote repository added or removed"
+    else
+        echo "Invalid input, Please enter y or n"
+        modify_remote
+    fi
+}
+modify_remote
+
 # check for unmodified files & uncommited changes
 files_add() {
     echo
@@ -153,9 +214,12 @@ files_add() {
         read -p "Enter the file name you want to add: " file_name
         git add $file_name
         echo
-        echo "$file_name added"
+        echo "$file_name added successfully"
         echo
         missed_add_fun
+        clear
+        echo "$file_name added successfully"
+        echo
     elif [ "$answer_add" = "n" ]; then
         clear
         echo "No files added"
@@ -186,66 +250,6 @@ commit() {
 }
 commit
 echo
-
-git remote
-git remote -v
-echo
-
-modify_remote() {
-    echo
-    another_remote_fun() {
-        read -p "DO you want to perform another remote repository modification? (y/n) " another_remote
-        if [ "$another_remote" = "y" ]; then
-            modify_remote
-        elif [ "$another_remote" = "n" ]; then
-            clear
-        else
-            echo
-            echo "Invalid input, Please enter y or n"
-            echo
-            another_remote_fun
-        fi
-    }
-
-    read -p "Do you want to add or remove a remote repository? (y/n) " answer_remote
-    if [ "$answer_remote" = "y" ]; then
-        read -p "what do you want to do (a)dd or (r)emove a remote repository? (a/r) " answer_add_remove
-        if [ "$answer_add_remove" = "a" ]; then
-            echo
-            read -p "Enter remote repository name you want to add: " remote_repository_name
-            read -p "Enter remote repository URL you want to add: " remote_repository_URL
-            echo
-            git remote add $remote_repository_name $remote_repository_URL
-            clear
-            echo "$remote_repository_name added"
-            another_remote_fun
-        elif [ "$answer_add_remove" = "r" ]; then
-            echo
-            read -p "Enter remote repository name you want to remove: " remote_repository_name
-            echo
-            git remote rm $remote_repository_name
-            clear
-            echo "$remote_repository_name removed"
-            another_remote_fun
-        else
-            echo "Invalid input, Please enter a or r"
-            modify_remote
-        fi
-
-        echo
-        git remote
-        git remote -v
-        echo
-
-    elif [ "$answer_remote" = "n" ]; then
-        clear
-        echo "No remote repository added or removed"
-    else
-        echo "Invalid input, Please enter y or n"
-        modify_remote
-    fi
-}
-modify_remote
 
 echo
 git status
