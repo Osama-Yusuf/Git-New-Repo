e219f5856df7857fdf5a8e6d26fd00bbb7410943
osama-yusuf <osama9mohamed5@gmail.com>
2022-06-27 17:49:09 +0200

    first commit

--- git_hub.sh
+++ git_hub.sh
@@ -1,8 +1,15 @@
 # dicover project files in a git repository
-git init
+read -p "Do you want to discover project files in a git repository? (y/n) " answer_init
+if [ "$answer_init" = "y" ]; then
+    git init
+else
+    echo 
+fi
 
 # check for unmodified files & uncommited changes
 git status
+
+echo
 
 # add all files to the repository
 read -p "Add all files to the repository? (y/n) " answer_add_all
@@ -20,13 +27,55 @@ else
     fi
 fi
 
+echo
 
 # commit the changes
-git commit -m "initial commit"
+read -p "Commit the changes? (y/n) " answer_commit
+if [ "$answer_commit" = "y" ]; then
+    read -p "Enter commit message: " commit_message
+    git commit -m "$commit_message"
+else
+    echo
+    echo "No changes committed"
+fi
+
+# check which branch you are on
+git branch
+
+echo
+
+read -p "Do you want to switch to a different branch? (y/n) " answer_switch
+if [ "$answer_switch" = "y" ]; then
+    read -p "Enter branch name: " branch_name
+    git checkout $branch_name
+else
+    echo
+fi
+
+echo
 
 # add the remote repository
-git remote add origin URL
+read -p "Add the remote repository? (y/n) " answer_add_remote
+if [ "$answer_add_remote" = "y" ]; then
+    read -p "Enter remote repository name: " remote_name
+    read -p "Enter remote repository url: " remote_url
+    git remote add $remote_name $remote_url
+else
+    echo
+    echo "No remote repository added"
+fi
+
+echo
 
 git status
 
-git push -u origin main
+echo
+
+# push new changes to github
+read -p "Push new changes to github? (y/n) " answer_push
+if [ "$answer_push" = "y" ]; then
+    git push -u $remote_url main
+else
+    echo
+    echo "No changes pushed to github"
+fi
